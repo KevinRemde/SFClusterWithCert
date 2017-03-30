b5fcce6baf67ba695190c45456b90ac114819592
Kevin Remde <kevin@remde.net>
2017-03-30 11:25:48 -0400

    cleanup

--- CreateSFCluster/DeploySFCluster.ps1
+++ CreateSFCluster/DeploySFCluster.ps1
@@ -18,16 +18,9 @@ $tenantID = $subscr.Tenant
 $init = Read-Host -Prompt "Please type your initials in lower case, and then press ENTER."
 
 
-# Prompt for the Azure region in which to build the lab machines
-
-<#
-Write-Host ""
-Write-Host "Where in the world do you want to put this?"
-Write-Host "Carefully enter 'East US' or 'West US'"
-$loc = Read-Host -Prompt "and then press ENTER."
-#>
-
+#
 # Variables 
+#
 
 $rgName = "RG-SFCluster" + $init
 # $deploymentName = $init + "AZLab"  # Not required
@@ -37,25 +30,12 @@ $rgName = "RG-SFCluster" + $init
 # $localAssets = "C:\Code\MyGitHub\SFClusterWithCert\Create SF Cluster\"
 # $templateFileLoc = $localAssets + "azuredeploy.json"
 # $parameterFileLoc = $localAssets + "azuredeploy.parameters.json"
-# $parameterFileLoc = $localAssets + "azuredeploy.parameters.json"
 
 # Use these if you want to drive the deployment from Github-based template. 
 #
-# $assetLocation = "https://rawgit.com/KevinRemde/20161115/master/" 
-# If the rawgit.com path is not available, you can try un-commenting the following line instead...
-# 
-# $assetLocation = "https://cgiresources.blob.core.windows.net/files/"
 $assetLocation = "https://raw.githubusercontent.com/KevinRemde/SFClusterWithCert/master/CreateSFCluster/"
 $templateFileURI  = $assetLocation + "azuredeploy.json"
 # $parameterFileURI = $assetLocation + "azuredeploy.parameters.json" # Use only if you want to use Kevin's defaults (not recommended)
-
-
-# Use Test-AzureRmDnsAvailability to create and verify unique DNS names.	
-#
-# Based on the initials entered, find unique DNS names for the four virtual machines.
-# NOTE: You may be wondering why I'm not also looking for unique storage account names.  
-# Those names are created by the template using randomly generated complex names, based on 
-# the resource group ID.
 
 $loc = "East US 2"
 
@@ -126,7 +106,8 @@ $keyvaultSecret = Set-AzureKeyVaultSecret -VaultName $vaultName -Name $secretNam
 
 $sourceVaultValue = "/subscriptions/"+ $subscr.Subscription.SubscriptionId +"/resourceGroups/"+$vaultRG+"/providers/Microsoft.keyVault/vaults/"+$vaultName
 
-# Get a unique DNS name
+# 
+# Use Test-AzureRmDnsAvailability to create and verify unique DNS name.	
 #
 $machine = $clusterName + "dns"
 $uniquename = $false
@@ -175,10 +156,8 @@ Measure-Command -expression { `
     -TemplateParameterObject $parameterObject `
     -Verbose}
 
-# use only if you want to use Kevin's default parameters (not recommended)
-# New-AzureRMResourceGroupDeployment -ResourceGroupName $rgName -TemplateUri $templateFileURI -TemplateParameterUri $parameterFileURI
 
-#Beep
+#Beep When done
 $([char]7)
 
 Write-Host ""
